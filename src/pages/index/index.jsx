0ce7450fe8df034ab335ba8769ec81e1b2790629
falstack <icesilt@outlook.com>
2019-11-16 09:17:53 +0800

    add: data request

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import { AtTabs, AtTabsPane, AtSearchBar } from 'taro-ui'
+import http from '~/utils/http'
 import './index.scss'
 
 export default class Index extends Component {
@@ -8,7 +9,31 @@ export default class Index extends Component {
     super(props)
     this.state = {
       tabActiveIndex: 0,
-      searchKeyword: ''
+      searchKeyword: '',
+      active: {
+        loading: false,
+        nothing: false,
+        noMore: false,
+        total: 0,
+        page: 1
+      },
+      release: {
+        loading: false,
+        nothing: false,
+        noMore: false,
+        total: 0,
+        page: 1
+      },
+      hottest: {
+        loading: false,
+        nothing: false,
+        noMore: false,
+        total: 0,
+        page: 1
+      },
+      list_0: [],
+      list_1: [],
+      list_2: [],
     }
   }
 
@@ -16,6 +41,90 @@ export default class Index extends Component {
     this.setState({
       tabActiveIndex
     })
+    if (this.state[`list_${tabActiveIndex}`].length) {
+      return
+    }
+    this.getIdols(tabActiveIndex)
+  }
+
+  componentDidMount() {
+    this.getIdols(0)
+  }
+
+  onPullDownRefresh() {
+    this.getIdols(this.state.tabActiveIndex)
+  }
+
+  onReachBottom() {
+    this.getIdols(this.state.tabActiveIndex)
+  }
+
+  getIdols(index, refresh = false) {
+    let sort = ''
+    if (index === 0) {
+      sort = 'active'
+    } else if (index === 1) {
+      sort = 'release'
+    } else {
+      sort = 'hottest'
+    }
+    const data = this.state[sort]
+    const field = `list_${index}`
+    if (data.loading || data.nothing || (data.noMore && !refresh)) {
+      return
+    }
+    if (refresh) {
+      this.setState({
+        [sort]: {
+          loading: true,
+          nothing: false,
+          noMore: false,
+          total: data.total
+        },
+        [field]: []
+      })
+    } else {
+      this.setState({
+        [sort]: {
+          ...data,
+          loading: true
+        }
+      })
+    }
+    http
+      .get('idol/list', {
+        sort,
+        take: 10,
+        page: data.page
+      })
+      .then(res => {
+        this.setState({
+          [field]: this.state[field].concat(res.list)
+        })
+        this.setState({
+          [sort]: {
+            loading: false,
+            nothing: false,
+            noMore: res.noMore,
+            total: res.total,
+            page: data.page + 1
+          },
+          [field]: this.state[field].concat(res.result)
+        })
+        if (refresh) {
+          Taro.stopPullDownRefresh()
+        }
+      })
+      .catch(() => {
+        this.setState({
+          [sort]: Object.assign(data, {
+            loading: false
+          })
+        })
+        if (refresh) {
+          Taro.stopPullDownRefresh()
+        }
+      })
   }
 
   handleSearchAction () {
@@ -37,16 +146,6 @@ export default class Index extends Component {
       searchKeyword
     })
   }
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
 
   render () {
     const tabList = [{ title: '动态' }, { title: '连载榜' }, { title: '总榜' }]
