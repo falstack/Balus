77351c2b6b4b52166a96223e77e99390c275ab58
falstack <icesilt@outlook.com>
2019-11-20 17:24:16 +0800

    update

--- src/pages/idol/show/index.jsx
+++ src/pages/idol/show/index.jsx
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
 import http from '~/utils/http'
+import helper from '~/utils/helper'
 import IdolPanel from './panel/IdolPanel'
 import IdolBottom from './bottom/IdolBottom'
 import './index.scss'
@@ -13,7 +14,10 @@ export default class extends Component {
       idol: {
         bangumi: {},
         lover: null
-      }
+      },
+      fans_total: 0,
+      fans_noMore: false,
+      fans_data: []
     }
   }
 
@@ -50,15 +54,47 @@ export default class extends Component {
       .catch(() => {})
   }
 
+  getIdolFans() {
+    http.get('idol/fans', {
+      slug: this.state.slug,
+      page: 0,
+      take: 5
+    })
+      .then(data => {
+        this.setState({
+          fans_total: data.total,
+          fans_noMore: data.no_more,
+          fans_data: data.result
+        })
+      })
+      .catch(() => {})
+  }
+
   componentDidMount () {
     this.getIdolData()
+    this.getIdolFans()
   }
 
   render () {
-    const { idol } = this.state
+    const { idol, fans_data } = this.state
+    const avatar = fans_data.map(user => (
+      <Image
+        className='avatar'
+        src={helper.resize(user.avatar, { width: 140 })}
+        key={user.slug}
+        taroKey={user.slug}
+        mode='aspectFit'
+      />
+    ))
     return (
       <View className='idol-show'>
         <IdolPanel idol={idol} />
+        <View className='intro'>
+          <Text className='intro__title'>{idol.bangumi.name} {idol.name}</Text>
+        </View>
+        <View className='intro avatar-list'>
+          {avatar}
+        </View>
         {
           idol.intro ?
             <View className='intro'>
