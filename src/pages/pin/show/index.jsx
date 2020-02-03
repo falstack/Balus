b5a1155bc2f07c683af955368f91cbd4e3f62f92
falstack <icesilt@outlook.com>
2020-02-03 10:46:17 +0800

    add: pin show

--- src/pages/pin/show/index.jsx
+++ src/pages/pin/show/index.jsx
@@ -1,28 +1,47 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { WebView } from '@tarojs/components'
+import http from '~/utils/http'
 import './index.scss'
 
 export default class extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      shareData: {
+        title: '萌市，二次元股市',
+        path: '/pages/index/index',
+        imageUrl: 'https://m1.calibur.tv/default-poster?imageMogr2/auto-orient/strip|imageView2/1/w/500/h/400'
+      }
+    }
   }
 
-  componentWillMount () { }
+  componentDidMount () {
+    this.getData()
+  }
 
-  componentDidMount () { }
+  onShareAppMessage() {
+    return this.state.shareData
+  }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  getData() {
+    http.get(`pin/show?slug=${this.$router.params.slug}`)
+      .then(data => {
+        this.setState({
+          shareData: {
+            title: `「${data.badge}」${data.title.text}`,
+            path: `/pages/pin/show/index?slug=${data.slug}`,
+            imageUrl: data.banner.length
+              ? `${data.banner[0].url}?imageMogr2/auto-orient/strip|imageView2/1/w/500/h/400`
+              : `${data.author.avatar}?imageMogr2/auto-orient/strip|imageView2/1/w/500/h/400`
+          }
+        })
+      })
+      .catch(() => {})
+  }
 
   render () {
     return (
-      <View>
-        <Text>text</Text>
-      </View>
+      <WebView src={`https://app.calibur.tv/pin/${this.$router.params.slug}`} />
     )
   }
 }
