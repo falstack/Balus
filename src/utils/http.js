9bd165590c6e44dca6eee229b17db5084b42aa0e
falstack <icesilt@outlook.com>
2020-03-09 10:00:08 +0800

    remove: flyio

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,43 +1,53 @@
-import Fly from 'flyio/dist/npm/wx'
 import cache from '~/utils/cache'
+import Taro from '@tarojs/taro'
 
-const fly = new Fly()
-const isProd = process.env.NODE_ENV === 'production'
-
-fly.interceptors.request.use(request => {
-  request.baseURL = isProd
-    ? 'https://api.calibur.tv/v1/'
-    : 'https://api.calibur.tv/v1/'
-    // : 'http://localhost:5200/v1/'
-  request.headers['Authorization'] = `Bearer ${cache.get('JWT-TOKEN')}`
-  request.timeout = 10000
-  return request
-})
-
-fly.interceptors.response.use(
-  res => res.data.data,
-  err => {
-    const code = err.status
-    const resp = {
-      code,
-      message: ''
+const request = (method, path, data, config) => {
+  return new Promise((resolve, reject) => {
+    const header = {
+      ...(config.header || {}),
+      'Authorization': `Bearer ${cache.get('JWT-TOKEN')}`
     }
-    if (!err.response) {
-      resp.message = '网路请求失败'
-    } else if (code === 0) {
-      resp.message = '网络不稳定！'
-    } else if (code === 1) {
-      resp.message = '网络太慢了！'
-    } else {
-      const { message } = err.response.data
-      if (typeof message === 'string') {
-        resp.message = message
-      } else {
-        resp.message = message[Object.keys(message)[0]][0]
+    const url = /^http/.test(path) ? path : `https://api.calibur.tv/v1/${path}`
+    Taro.request({
+      ...config,
+      url,
+      data,
+      header,
+      method,
+      success: res => {
+        resolve(res.data.data)
+      },
+      fail: err => {
+        const code = err.status
+        const resp = {
+          code,
+          message: '网络错误'
+        }
+        try {
+          resp.message = err.response.data.message || ''
+          resp.message = resp.message || '网络错误'
+          if (code === 0) {
+            resp.message = '网络不稳定！'
+          } else if (code === 1) {
+            resp.message = '网络太慢了！'
+          }
+        } catch (e) {
+          // do nothing
+        }
+        reject(resp)
       }
-    }
-    return Promise.reject(resp)
-  }
-)
+    })
+  })
+}
 
-export default fly
+const Http = {
+  get(url, params, config = {}) {
+    return request('GET', url, params, config)
+  },
+
+  post(url, params, config = {}) {
+    return request('POST', url, params, config)
+  }
+}
+
+export default Http
