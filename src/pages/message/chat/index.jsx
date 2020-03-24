27adac38720ad42f81b032dad974acc437bc2005
falstack <icesilt@outlook.com>
2020-03-24 09:14:11 +0800

    add: input

--- src/pages/message/chat/index.jsx
+++ src/pages/message/chat/index.jsx
@@ -1,21 +1,25 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Input } from '@tarojs/components'
 import ChatList from '~/components/FlowList/ChatList'
 import './index.scss'
 
 export default class extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
+  config = {
+    disableScroll: true
   }
 
-  componentDidMount () { }
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  handleInput(evt) {
+    this.setState({
+      value: evt.detail.value
+    })
+  }
 
   render () {
     const { channel } = this.$router.params
@@ -23,9 +27,27 @@ export default class extends Component {
       return
     }
 
+    const { value } = this.state
+
     return (
-      <View>
-        <ChatList slug={channel} />
+      <View className='message-chat scroll-page'>
+        <View className='flex-grow-1'>
+          <View className='scroll-wrap'>
+            <ChatList slug={channel} />
+          </View>
+        </View>
+        <View className='flex-shrink-0 input-wrap'>
+          <View className='input-box'>
+            <Input
+              value={value}
+              confirmHold
+              type='text'
+              placeholder='输入新消息'
+              confirmType='send'
+              onInput={this.handleInput}
+            />
+          </View>
+        </View>
       </View>
     )
   }
