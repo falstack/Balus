829899f0cc7b6459c26c87b8a88b60523c024926
falstack <icesilt@outlook.com>
2020-04-27 08:20:11 +0800

    save

--- src/components/FlowItem/UnreadCommentItem/index.jsx
+++ src/components/FlowItem/UnreadCommentItem/index.jsx
@@ -1,26 +1,69 @@
 import Taro, { PureComponent } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image, Block } from '@tarojs/components'
+import utils from '~/utils'
 import './index.scss'
 
-class UnreadCommentItem extends PureComponent {
+class UnreadAgreeItem extends PureComponent {
   constructor (props) {
     super(props)
     this.state = {}
   }
 
+  clickBody() {
+    const { data } = this.props.item
+    Taro.navigateTo({
+      url: `/pages/pin/show/index?slug=${data.pin_slug}`,
+    })
+  }
+
+  clickUser() {
+    const { user } = this.props.item
+    this.$preload('user', user)
+    Taro.navigateTo({
+      url: `/pages/user/show/index?slug=${user.slug}`,
+    })
+  }
+
   render () {
     const { item } = this.props
-
+    const isPin = !item.comment.getter
     return (
       <View className='u-c-item'>
-        {item}
+        <View className='header'>
+          <Image onClick={this.clickUser.bind(this)} className='avatar' src={utils.resize(item.comment.author.avatar, { width: 80 })} />
+          <View className='info'>
+            <View className='name-wrap'>
+              <Text className='nickname' onClick={this.clickUser.bind(this)}>{item.comment.author.nickname}</Text>
+              <Text>回复了你的{isPin ? '文章' : '评论'}</Text>
+            </View>
+            <View className='meta'>
+              {utils.timeAgo(item.comment.created_at)}
+            </View>
+          </View>
+        </View>
+        <View className='body' onClick={this.clickBody.bind(this)}>
+          <View className='text-wrap'>
+            <View className='content'>{item.comment.content[0].data.text}</View>
+            <View className='meta'>
+              <Text className='item'>{item.comment.like_count} 喜欢</Text>
+            </View>
+          </View>
+        </View>
+        <View className='footer' />
       </View>
     )
   }
 }
 
-UnreadCommentItem.defaultProps = {
-  item: {}
+UnreadAgreeItem.defaultProps = {
+  item: {
+    comment: {
+      author: {}
+    },
+    pin: {
+      banner: []
+    }
+  }
 }
 
-export default UnreadCommentItem
+export default UnreadAgreeItem
--- src/components/FlowItem/UnreadRewardItem/index.jsx
+++ src/components/FlowItem/UnreadRewardItem/index.jsx
@@ -1,5 +1,6 @@
 import Taro, { PureComponent } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
+import utils from '~/utils'
 import './index.scss'
 
 class UnreadRewardItem extends PureComponent {
@@ -8,19 +9,69 @@ class UnreadRewardItem extends PureComponent {
     this.state = {}
   }
 
+  clickBody() {
+    const { pin } = this.props.item
+    this.$preload('pin', pin)
+    Taro.navigateTo({
+      url: `/pages/pin/show/index?slug=${pin.slug}`,
+    })
+  }
+
+  clickUser() {
+    const { user } = this.props.item
+    this.$preload('user', user)
+    Taro.navigateTo({
+      url: `/pages/user/show/index?slug=${user.slug}`,
+    })
+  }
+
   render () {
     const { item } = this.props
 
     return (
       <View className='u-r-item'>
-        {item}
+        <View className='header'>
+          <Image onClick={this.clickUser.bind(this)} className='avatar' src={utils.resize(item.user.avatar, { width: 80 })} />
+          <View className='info'>
+            <View className='name-wrap'>
+              <Text className='nickname' onClick={this.clickUser.bind(this)}>{item.user.nickname}</Text>
+              <Text>投食了你的文章</Text>
+            </View>
+            <View className='meta'>
+              {utils.timeAgo(item.created_at)}
+            </View>
+          </View>
+        </View>
+        <View className='body' onClick={this.clickBody.bind(this)}>
+          {
+            item.pin.banner.length
+              ? <Image className='banner' src={utils.resize(item.pin.banner[0], { width: 100, height: 80 })} />
+              : ''
+          }
+          <View className='text-wrap'>
+            <View className='content'>{item.pin.title.text}</View>
+            <View className='meta'>
+              <Text className='item'>{item.pin.like_count} 喜欢</Text>
+              <Text className='dot'>·</Text>
+              <Text className='item'>{item.pin.comment_count} 评论</Text>
+              <Text className='dot'>·</Text>
+              <Text className='item'>{item.pin.mark_count} 收藏</Text>
+            </View>
+          </View>
+        </View>
+        <View className='footer' />
       </View>
     )
   }
 }
 
 UnreadRewardItem.defaultProps = {
-  item: {}
+  item: {
+    user: {},
+    pin: {
+      banner: []
+    }
+  }
 }
 
 export default UnreadRewardItem
